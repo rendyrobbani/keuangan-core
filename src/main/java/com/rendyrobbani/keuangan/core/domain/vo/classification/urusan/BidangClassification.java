a87c1ee5896aabc180f88b02163b6314180bdfb9
Rendy Robbani <rendyrobbani17@gmail.com>
2025-08-18 20:30:35 +0800

    Development

---
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/vo/classification/urusan/BidangClassification.java
@@ -0,0 +1,28 @@
+package com.rendyrobbani.keuangan.core.domain.vo.classification.urusan;
+
+import com.rendyrobbani.keuangan.core.common.util.regex.RegexUtil;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(fluent = true)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@SuppressWarnings("ClassCanBeRecord")
+public final class BidangClassification {
+
+	private final String urusanCode;
+
+	private final String bidangCode;
+
+	public static final String REGEX = "^(?!X\\.[0-9]{2}.*|[0-9]\\.XX.*|0.*|..00.*$)([X1-8])\\.(?!00$)([X0-9]{2})$";
+
+	public static BidangClassification parse(String code) {
+		if (!code.matches(REGEX)) throw new RuntimeException("Invalid code");
+		var groups = RegexUtil.groups(code, REGEX);
+		return new BidangClassification(String.join(".", groups.subList(0, 1)),
+		                                String.join(".", groups.subList(0, 2)));
+	}
+
+}
---
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/vo/classification/urusan/UrusanClassification.java
@@ -0,0 +1,23 @@
+package com.rendyrobbani.keuangan.core.domain.vo.classification.urusan;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(fluent = true)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@SuppressWarnings("ClassCanBeRecord")
+public final class UrusanClassification {
+
+	private final String urusanCode;
+
+	public static final String REGEX = "^[X1-8]$";
+
+	public static UrusanClassification parse(String code) {
+		if (code.matches(REGEX)) throw new RuntimeException("Invalid code");
+		return new UrusanClassification(code);
+	}
+
+}
