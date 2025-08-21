aa020d5fff29f8d3463b7eb99c90ca2960c72fe1
Rendy Robbani <rendyrobbani17@gmail.com>
2025-08-21 08:55:02 +0800

    Development

---
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/entity/master/classification/skpd/DataMasterSkpd.java
@@ -0,0 +1,5 @@
+package com.rendyrobbani.keuangan.core.domain.entity.master.classification.skpd;
+
+public interface DataMasterSkpd {
+
+}
--- src/main/java/com/rendyrobbani/keuangan/core/domain/repository/master/classification/organisasi/DataMasterOrganisasiRepository.java
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/repository/master/classification/organisasi/DataMasterOrganisasiRepository.java
@@ -3,6 +3,18 @@ package com.rendyrobbani.keuangan.core.domain.repository.master.classification.o
 import com.rendyrobbani.keuangan.core.domain.entity.master.classification.organisasi.DataMasterOrganisasi;
 import com.rendyrobbani.keuangan.core.domain.repository.master.classification.DataMasterClassificationRepository;
 
+import java.util.List;
+
 public interface DataMasterOrganisasiRepository extends DataMasterClassificationRepository<DataMasterOrganisasi> {
+
+	List<DataMasterOrganisasi> findByIsSkpd();
+
+	List<DataMasterOrganisasi> findByIsUnit();
+
+	List<DataMasterOrganisasi> findByIsUnitAndSkpdId(String skpdId);
+
+	DataMasterOrganisasi findByIsSetda();
+
+	DataMasterOrganisasi findByIsSkpkd();
 
 }
