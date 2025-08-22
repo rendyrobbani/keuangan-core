0c0c838815690991555019c25a0e28b77097ed9f
Rendy Robbani <rendyrobbani17@gmail.com>
2025-08-22 18:28:51 +0800

    Development

--- src/main/java/com/rendyrobbani/keuangan/core/domain/repository/master/komponen/DataMasterKomponenRepository.java
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/repository/master/komponen/DataMasterKomponenRepository.java
@@ -2,22 +2,23 @@ package com.rendyrobbani.keuangan.core.domain.repository.master.komponen;
 
 import com.rendyrobbani.keuangan.core.domain.entity.master.komponen.DataMasterKomponen;
 import com.rendyrobbani.keuangan.core.domain.repository.master.DataMasterRepository;
+import com.rendyrobbani.keuangan.core.domain.vo.KomponenType;
 
 import java.util.List;
 
 public interface DataMasterKomponenRepository extends DataMasterRepository<DataMasterKomponen, Long> {
 
-	List<DataMasterKomponen> findByType(String type);
+	List<DataMasterKomponen> findByType(KomponenType type);
 
-	List<DataMasterKomponen> findByTypeAndIsDeletedFalse(String type);
+	List<DataMasterKomponen> findByTypeAndIsDeletedFalse(KomponenType type);
 
-	List<DataMasterKomponen> findByTypeAndIsDeletedTrue(String type);
+	List<DataMasterKomponen> findByTypeAndIsDeletedTrue(KomponenType type);
 
-	List<DataMasterKomponen> findByTypeAndRekeningId(String type, String rekeningId);
+	List<DataMasterKomponen> findByTypeAndRekeningId(KomponenType type, String rekeningId);
 
-	List<DataMasterKomponen> findByTypeAndRekeningIdAndIsDeletedFalse(String type, String rekeningId);
+	List<DataMasterKomponen> findByTypeAndRekeningIdAndIsDeletedFalse(KomponenType type, String rekeningId);
 
-	List<DataMasterKomponen> findByTypeAndRekeningIdAndIsDeletedTrue(String type, String rekeningId);
+	List<DataMasterKomponen> findByTypeAndRekeningIdAndIsDeletedTrue(KomponenType type, String rekeningId);
 
 	List<DataMasterKomponen> findByRekeningId(String rekeningId);
 
