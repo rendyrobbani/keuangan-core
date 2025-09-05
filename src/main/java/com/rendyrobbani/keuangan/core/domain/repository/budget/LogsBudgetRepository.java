fc8c0f501bf30918ec769162086a653ea1a70cbf
Rendy Robbani <rendyrobbani17@gmail.com>
2025-09-06 00:05:22 +0800

    Development

--- src/main/java/com/rendyrobbani/keuangan/core/domain/repository/budget/LogsBudgetRepository.java
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/repository/budget/LogsBudgetRepository.java
@@ -4,11 +4,14 @@ import com.rendyrobbani.keuangan.core.domain.entity.budget.DataBudgetEntity;
 import com.rendyrobbani.keuangan.core.domain.entity.budget.LogsBudgetEntity;
 import com.rendyrobbani.keuangan.core.domain.repository.Repository;
 
-public interface LogsBudgetRepository<
-		ENTITY extends LogsBudgetEntity<SUBJECT, SUBJECTID>,
-		SUBJECT extends DataBudgetEntity<SUBJECTID>,
-		SUBJECTID
-		>
-		extends Repository<ENTITY, Long> {
+import java.util.List;
+
+public interface LogsBudgetRepository<ENTITY extends LogsBudgetEntity<SUBJECT, SUBJECTID>, SUBJECT extends DataBudgetEntity<SUBJECTID>, SUBJECTID> extends Repository<ENTITY, Long> {
+
+	List<ENTITY> findBySubjectId(SUBJECTID subjectId);
+
+	List<ENTITY> findBySubjectIdAndIsDeletedFalse(SUBJECTID subjectId);
+
+	List<ENTITY> findBySubjectIdAndIsDeletedTrue(SUBJECTID subjectId);
 
 }
