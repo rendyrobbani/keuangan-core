98070633685c988564a6e880622e509ded9ed53f
Rendy Robbani <rendyrobbani17@gmail.com>
2025-09-12 22:11:12 +0800

    Development

--- src/main/java/com/rendyrobbani/keuangan/core/domain/repository/budget/record/tapd/sign/DataBudgetRecordTapdSignRepository.java
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/repository/budget/record/tapd/sign/DataBudgetRecordTapdSignRepository.java
@@ -1,8 +1,9 @@
 package com.rendyrobbani.keuangan.core.domain.repository.budget.record.tapd.sign;
 
 import com.rendyrobbani.keuangan.core.domain.entity.budget.record.tapd.sign.DataBudgetRecordTapdSign;
+import com.rendyrobbani.keuangan.core.domain.entity.master.tapd.sign.DataMasterTapdSign;
 import com.rendyrobbani.keuangan.core.domain.repository.budget.record.DataBudgetRecordRepository;
 
-public interface DataBudgetRecordTapdSignRepository extends DataBudgetRecordRepository<DataBudgetRecordTapdSign, DataBudgetRecordTapdSign, String> {
+public interface DataBudgetRecordTapdSignRepository extends DataBudgetRecordRepository<DataBudgetRecordTapdSign, DataMasterTapdSign, String> {
 
 }
