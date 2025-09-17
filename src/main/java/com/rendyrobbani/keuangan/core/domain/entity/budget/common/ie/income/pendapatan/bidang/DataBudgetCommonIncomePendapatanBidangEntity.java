f3c7d816d94dcff83de007d7cedc5bbd966a8627
Rendy Robbani <rendyrobbani17@gmail.com>
2025-09-17 09:59:36 +0800

    Development

---
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/entity/budget/common/ie/income/pendapatan/bidang/DataBudgetCommonIncomePendapatanBidangEntity.java
@@ -0,0 +1,67 @@
+package com.rendyrobbani.keuangan.core.domain.entity.budget.common.ie.income.pendapatan.bidang;
+
+import com.rendyrobbani.keuangan.core.domain.entity.budget.common.ie.DataBudgetCommonBidangEntity;
+import com.rendyrobbani.keuangan.core.domain.entity.master.classification.bidang.DataMasterBidang;
+import com.rendyrobbani.keuangan.core.domain.entity.master.classification.organisasi.DataMasterOrganisasi;
+import com.rendyrobbani.keuangan.core.domain.entity.master.classification.urusan.DataMasterUrusan;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public interface DataBudgetCommonIncomePendapatanBidangEntity extends DataBudgetCommonBidangEntity {
+
+	@Override
+	String id();
+
+	@Override
+	String skpdId();
+
+	@Override
+	String unitId();
+
+	@Override
+	String urusanId();
+
+	@Override
+	String bidangId();
+
+	@Override
+	BigDecimal sebelum();
+
+	@Override
+	BigDecimal setelah();
+
+	@Override
+	LocalDateTime createdAt();
+
+	@Override
+	String createdBy();
+
+	@Override
+	LocalDateTime updatedAt();
+
+	@Override
+	String updatedBy();
+
+	@Override
+	boolean isDeleted();
+
+	@Override
+	LocalDateTime deletedAt();
+
+	@Override
+	String deletedBy();
+
+	@Override
+	DataMasterOrganisasi skpd();
+
+	@Override
+	DataMasterOrganisasi unit();
+
+	@Override
+	DataMasterUrusan urusan();
+
+	@Override
+	DataMasterBidang bidang();
+
+}
