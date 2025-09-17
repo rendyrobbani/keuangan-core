e4499c8aac9381f824068f1c1e534eba295269cc
Rendy Robbani <rendyrobbani17@gmail.com>
2025-09-17 09:56:51 +0800

    Development

---
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/entity/budget/common/ie/income/pendapatan/organisasi/DataBudgetCommonIncomePendapatanOrganisasiEntity.java
@@ -0,0 +1,53 @@
+package com.rendyrobbani.keuangan.core.domain.entity.budget.common.ie.income.pendapatan.organisasi;
+
+import com.rendyrobbani.keuangan.core.domain.entity.budget.common.ie.DataBudgetCommonOrganisasiEntity;
+import com.rendyrobbani.keuangan.core.domain.entity.master.classification.organisasi.DataMasterOrganisasi;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public interface DataBudgetCommonIncomePendapatanOrganisasiEntity extends DataBudgetCommonOrganisasiEntity {
+
+	@Override
+	String id();
+
+	@Override
+	String skpdId();
+
+	@Override
+	String unitId();
+
+	@Override
+	BigDecimal sebelum();
+
+	@Override
+	BigDecimal setelah();
+
+	@Override
+	LocalDateTime createdAt();
+
+	@Override
+	String createdBy();
+
+	@Override
+	LocalDateTime updatedAt();
+
+	@Override
+	String updatedBy();
+
+	@Override
+	boolean isDeleted();
+
+	@Override
+	LocalDateTime deletedAt();
+
+	@Override
+	String deletedBy();
+
+	@Override
+	DataMasterOrganisasi skpd();
+
+	@Override
+	DataMasterOrganisasi unit();
+
+}
