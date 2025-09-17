609cb6f9db34fe67fed586010b0ddfe0a96f2830
Rendy Robbani <rendyrobbani17@gmail.com>
2025-09-17 16:07:15 +0800

    Development

---
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/entity/budget/common/ie/income/pendapatan/rincian/SelfBudgetCommonIncomePendapatanRincian.java
@@ -0,0 +1,9 @@
+package com.rendyrobbani.keuangan.core.domain.entity.budget.common.ie.income.pendapatan.rincian;
+
+import com.rendyrobbani.keuangan.core.domain.entity.SelfEntity;
+
+public interface SelfBudgetCommonIncomePendapatanRincian extends SelfEntity {
+
+	String TABLE_NAME = "self_budget_common_income_pendapatan_rincian";
+
+}
