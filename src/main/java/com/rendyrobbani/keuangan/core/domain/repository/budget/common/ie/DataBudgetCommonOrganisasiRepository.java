506663dff4e1030ac725892764720b84f793d33d
Rendy Robbani <rendyrobbani17@gmail.com>
2025-09-22 13:21:30 +0800

    Development

--- src/main/java/com/rendyrobbani/keuangan/core/domain/repository/budget/common/ie/DataBudgetCommonOrganisasiRepository.java
+++ src/main/java/com/rendyrobbani/keuangan/core/domain/repository/budget/common/ie/DataBudgetCommonOrganisasiRepository.java
@@ -4,11 +4,20 @@ import com.rendyrobbani.keuangan.core.domain.entity.budget.common.ie.DataBudgetC
 import com.rendyrobbani.keuangan.core.domain.repository.budget.common.DataBudgetCommonRepository;
 
 import java.sql.Connection;
+import java.time.LocalDateTime;
 
 public interface DataBudgetCommonOrganisasiRepository<ENTITY extends DataBudgetCommonOrganisasiEntity> extends DataBudgetCommonRepository<ENTITY, String> {
 
-	void deleteAll(Connection connection);
+	void deleteAll(Connection connection, LocalDateTime deletedAt, String deletedBy);
 
-	void updateAll(Connection connection);
+	void updateAll(Connection connection, LocalDateTime updatedAt, String updatedBy);
+
+	void deleteBySkpdId(Connection connection, LocalDateTime deletedAt, String deletedBy, String skpdId);
+
+	void updateBySkpdId(Connection connection, LocalDateTime updatedAt, String updatedBy, String skpdId);
+
+	void deleteByUnitId(Connection connection, LocalDateTime deletedAt, String deletedBy, String unitId);
+
+	void updateByUnitId(Connection connection, LocalDateTime updatedAt, String updatedBy, String unitId);
 
 }
